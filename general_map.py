72c95498361e744eeb818495e9568b5e877275b1
samgagnon <37914169+samgagnon@users.noreply.github.com>
2021-05-18 16:49:21 -0400

    Still need to fix the coordinate bug

--- general_map.py
+++ general_map.py
@@ -8,19 +8,19 @@ from datetime import datetime
 from bns import *
 
 
-def get_time_and_band(instrument='MMT'):
+def get_time_and_band(api_token, graceid, instrument='MMT'):
     """
     Returns times, bands, and depths related to an instrument.
     """
     BASE = 'http://treasuremap.space/api/v0'
-    api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
+    # api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
     
     TARGET = 'pointings'
 
     #define the filtering parameters 
     #grab all of the completed pointings taken with a specific instrument for a given event
 
-    graceid = 'S190425z'
+    # graceid = 'S190425z'
 
     params = {
         "api_token":api_token,
@@ -31,7 +31,7 @@ def get_time_and_band(instrument='MMT'):
 
     url = "{}/{}?{}".format(BASE, TARGET, urllib.parse.urlencode(params))
     r = requests.get(url = url)
-    print("There are %s pointings" % len(json.loads(r.text)))
+    # print("There are %s pointings" % len(json.loads(r.text)))
 
     #print the first
     # print(json.loads(r.text))
@@ -58,13 +58,15 @@ def get_time_and_band(instrument='MMT'):
     return time, band, [depths, depth_units, depth_errs]
 
 
-def get_pointings():
-
+def get_pointings(api_token, graceid):
+    """
+    Gets all EM pointings for a particular event
+    """
     BASE = 'http://treasuremap.space/api/v0/'
     TARGET = 'pointings'
-    api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
+    # api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
 
-    payload = {'graceid':'S190425z', 'api_token':api_token}
+    payload = {'graceid':graceid, 'api_token':api_token}
     r = requests.get(url = BASE+TARGET, params = payload)
     results = r.json()
 
@@ -80,15 +82,15 @@ def get_pointings():
     return np.asarray(pointings)
 
 
-def get_footprints():
+def get_footprints(api_token, graceid):
     """
-    Gets the footprints for each instrument
+    Gets the footprints for each instrumen
     """
     BASE = 'http://treasuremap.space/api/v0/'
     TARGET='footprints'
-    api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
+    # api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
 
-    payload = {'graceid':'S190425z', 'api_token':api_token}
+    payload = {'graceid':graceid, 'api_token':api_token}
     r = requests.get(url = BASE+TARGET, params = payload)
     results = r.json()
 
@@ -109,12 +111,12 @@ def get_footprints():
     return footprints_dict
 
 
-def get_instruments():
+def get_instruments(api_token, graceid):
     BASE = 'http://treasuremap.space/api/v0/'
     TARGET = 'instruments'
-    api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
+    # api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
 
-    payload = {'graceid':'S190425z', 'api_token':api_token}
+    payload = {'graceid':graceid, 'api_token':api_token}
     r = requests.get(url = BASE+TARGET, params = payload)
     results = r.json()
 
@@ -135,7 +137,6 @@ def get_inst_pointings(pointings):
     MMT_pointings = []
     CSS_pointings = []
     GOTO_pointings = []
-    print(np.asarray(pointings))
     for p in range(len(pointings)):
         if pointings[p][0] == 22:
             MMT_pointings.append(pointings[p])
@@ -144,16 +145,15 @@ def get_inst_pointings(pointings):
         if pointings[p][0] == 11:
             CSS_pointings.append(pointings[p])
     MMT_pointings = np.array(MMT_pointings)[:,1:3]
-    print(MMT_pointings)
     CSS_pointings = np.array(CSS_pointings)[:,1:3]
     GOTO_pointings = np.array(GOTO_pointings)[:,1:3]
     return MMT_pointings, CSS_pointings, GOTO_pointings
 
 
-def get_important_things(instrument):
+def get_important_things(api_token, graceid, instrument):
     # for a given instrument, returns the median time of the pointing
     # since the event as well as the band
-    time, band, depth_list = get_time_and_band(instrument)
+    time, band, depth_list = get_time_and_band(api_token, graceid, instrument)
     event = '2019-04-25T08:18:05'
     FMT = '%Y-%m-%dT%H:%M:%S'
     time = [datetime.strptime(t[:19], FMT) - datetime.strptime(event, FMT) for t in time]
@@ -194,56 +194,55 @@ def query_footprint(footprint, pointing):
     return hp.query_polygon(256, polygon)
 
 
-def coverage_in_time(ti, tf, instrument='css'):
+def coverage_in_time(ti, tf, info_list, event_loc):
     """
     Returns total probability coverage during time period
     ti: initial time in days
     tf: final time in days
-    instrument: either goto or css, the instrument dictionary
+    info_list: the information list
     """
-    if instrument == 'css':
-        footprint = footprints_dict.get(11)[:-1]
-        instrument = css
-    elif instrument == 'goto':
-        footprint = footprints_dict.get(71)[:-1]
-        instrument = goto
-    pointings = instrument['pointings']
-    times = instrument['time']
-    depths = instrument['depth']
-    indices = []
-    for i in range(len(times)):
-        if ti <= times[i] <= tf:
-            indices.append(i)
-    pointings = pointings[indices]
-    idx = []
-    index = 0
-    point_n = []
-    for pointing in pointings:
-        new_indices = list(query_footprint(footprint, pointing))
-        idx = idx + new_indices
-        point_n = point_n + [index] * len(new_indices)
-        index += 1
-    ids = np.array([np.asarray(idx), np.asarray(point_n)]) 
-    a, x = len(ids[0]), 0
-    while x < a:
-        list_trunc = list(ids[0])[:x]
-        if ids[0][x] in list_trunc:
-            ids = np.delete(ids, x, 1)
-        x += 1
-        a = len(ids[0])
-    #     idx = list(dict.fromkeys(idx)) # kills duplicates
-    pdist = hpx[ids[0].astype(np.int)]
-    p = pdist.sum()
-    if ids[0].size:
-        ptgid = choices(ids[1], pdist)
-        depth = depths[ptgid]
-    else:
-        depth = 0.0
-    return p, idx, depth
+    results = []
+    for info in info_list:
+        footprint = footprints_dict.get(info['ID'])[:-1]
+        pointings = info['pointings']
+        print(pointings)
+        times = info['time']
+        depths = info['depth']
+        band = info['band']
+        indices = []
+        for i in range(len(times)):
+            if ti <= times[i] <= tf:
+                indices.append(i)
+        # print(tf, indices)
+        if indices == []:
+            # if there are no pointings for this instrument,
+            # try next instrument
+            continue
+        pointings = pointings[indices]
+        idx = []
+        index = 0
+        point_n = []
+        print(pointings)
+        for pointing in pointings:
+            # loops through pointings and appends indices covered by
+            # the instrument footprint
+            # print(pointing)
+            new_indices = list(query_footprint(footprint, pointing))
+            # dpx = np.log10(hpx)
+            # dpx[new_indices] *= 5e5
+            # hp.visufunc.mollview(dpx)
+            # plt.show()
+            if event_loc in new_indices:
+                print(True)
+                depth = depths[event_loc]
+                band = bands[event_loc]
+                inst_name = info["inst_name"]
+                results += [inst_name, band, depth]
+    return results
 
 
 
-def coverage_iterator(time, instrument):
+def coverage_iterator(hpx, time, info_list, event_loc):
     """
     Given a list describing the number of time windows,
     the beginning of the first window and the end of the last,
@@ -252,99 +251,71 @@ def coverage_iterator(time, instrument):
     - time: time list [dt, ti, tf]
     - instrument: instrument string
     """
-    global hpx
-    hpx, header = hp.read_map('LALInference.fits.gz,0', h=True, verbose=False)
     ti = time[1]
     tf = time[2]
     nt = time[0]
     dt = (tf - ti) / nt
     t2 = ti + dt
+    detections = []
     for i in range(int(nt)):
         ti += dt * i
         t2 += dt * i
-        p, idx, depth = coverage_in_time(ti, t2, instrument)
-        r = np.random.uniform()
-        if r <= p:
-            return [ti, t2], i, depth
+        # print("Calling coverage in time", i)
+        results = coverage_in_time(ti, t2, info_list, event_loc)
+        if results == []:
+            continue
         else:
-            hpx[idx] = 0
-            hpx /= hpx.sum()
-    return None
+            for result in result:
+                result += [ti, t2, i,]
+            detections += results
+    return detections
 
 
-def convert_flux(flux, wavelength):
-    # adjust flux for viewing distance
-    dpc = 157.965e6
-    flux *= (10/dpc)**2
-    # c = 299792458e7 # speed of light in angstroms per second
-    flux_density = 3.34e4 * flux * (wavelength ** 2)
-    return flux_density
-
-
-def main(time, instrument, bns):
+def sim_event(api_token, graceid, mapfile, time):
+    """
+    Runs multiple simulations for a particular event 
+    """
     global pointings, footprints_dict, instrument_dict, mmt, css, goto
-    pointings = get_pointings()
-    footprints_dict = get_footprints()
-    instrument_dict = get_instruments()
+    global hpx
+    # load in GW localization
+    hpx = hp.read_map(mapfile, verbose=False)
+    print(hp.pixelfunc.get_nside(hpx))
+    hpx = hp.pixelfunc.ud_grade(hpx, 256)
+    # hp.visufunc.cartview(hpx)
+    # plt.show()
+    # print(hpx)
+    event_loc = choices(list(range(len(hpx))), hpx)
+    print(event_loc, hpx[event_loc])
+    print(hp.pixelfunc.pix2ang(256, event_loc, lonlat=True))
+    pointings = get_pointings(api_token, graceid)
+    footprints_dict = get_footprints(api_token, graceid)
+    # instrument_dict gives the name associated with each instrument ID
+    instrument_dict = get_instruments(api_token, graceid)
     pointings = np.delete(pointings, 1, 1)
     # get the ids for each pointing class
+    print(instrument_dict)
     id_set = set(list(pointings[:, 0]))
-    # instrument_dict gives the name associated with each instrument ID
-    # MMT_pointings, CSS_pointings, GOTO_pointings = get_inst_pointings(pointings) deprecated
-    # mmt_time, mmt_band, mmt_depth = get_important_things('MMT') dep
-    # goto_time, goto_band, goto_depth = get_important_things('GOTO-4') dep
-    # css_time, css_band, css_depth = get_important_things('CSS') dep
+    legal_inst = [38, 22, 23, 24, 25, 26, 11, 65, 52, 47, 70, 71, 12]
+    # I removed the Swift Burst Alert Telescope due to a polygon issue with 
+    # its footprint
     info_list = []
     for id_ in id_set:
-        time, band, depth = get_important_things(instrument_dict[id_])
+        if id_ not in legal_inst:
+            continue
+        # construct list of dictionaries with info for each instrument
+        ob_time, band, depth = get_important_things(api_token, \
+                               graceid, instrument_dict[id_])
         info = {}
-        info['pointings'] = pointings[pointings[:,0] == id_]
-        info['time'] = np.array(time)
+        info['ID'] = id_
+        info['inst_name'] = instrument_dict[id_]
+        info['pointings'] = pointings[pointings[:,0] == id_][:,1:]
+        info['time'] = np.array(ob_time)
         info['band'] = np.array(band)
         info['depth'] = np.array(depth[0])
-        info_list += info
-    print(info_list)
-    # mmt = {}
-    # mmt['pointings'] = MMT_pointings
-    # mmt['time'] = np.array(mmt_time)
-    # mmt['band'] = np.array(mmt_band)
-    # mmt['depth'] = np.array(mmt_depth[0])
-    # css = {}
-    # css['pointings'] = CSS_pointings
-    # css['time'] = np.array(css_time)
-    # css['band'] = np.array(css_band)
-    # css['depth'] = np.array(css_depth[0])
-    # goto = {}
-    # goto['pointings'] = GOTO_pointings
-    # goto['time'] = np.array(goto_time)
-    # goto['band'] = np.array(goto_band)
-    # goto['depth'] = np.array(goto_depth[0])
-    detection = coverage_iterator(time, instrument)
-    pos_angles = []
-    if detection is None:
-        print("No detection made")
-        return [0]
-    else:
-        [t1, t2], i, depth = detection
-        # convert depth to correct flux density
-        if instrument == 'goto':
-            r1, r2 = 3360, 5420 # units are angstroms
-        [t1, t2], i, depth = detection
-        wavecollection = [bns['oa0.1'], bns['oa0.2'], bns['oa0.3'], 
-            bns['oa0.4'], bns['oa0.5'], bns['oa0.6'], bns['oa0.7'],
-            bns['oa0.8'], bns['oa0.9'], bns['oa1.0']]
-        angle = 0.1
-        for wave in wavecollection:
-            flux = 0
-            for waveform in wave:
-                if r1 < waveform[0] < r2:
-                    flux += convert_flux(waveform[i], waveform[0])
-            mAB = 8.9 - 2.5 * np.log10(flux)
-            print(mAB, depth)
-            if mAB < depth:
-                pos_angles.append(angle)
-            angle += 0.1
-        return pos_angles
+        info_list += [info]
+    # given a time window and the pointing info, determine if a detection is made
+    detections = coverage_iterator(hpx, time, info_list, event_loc)
+    return detections
 
         
 
@@ -352,9 +323,12 @@ def main(time, instrument, bns):
 if __name__ == "__main__":
     bns = get_bns()
     time = bns['time']
-    angles = []
-    for i in range(100):
-        a = main([int(time[0]), int(time[1]), int(time[2])], 'goto', bns)
-        if a != [0]:
-            angles = angles + a
-    np.savetxt('angles_histogram', np.asarray(angles))
+    api_token = "zd6DGajyV66nlxuGhWt4CzIbdJ3wwr2UzlbkEg"
+    graceid = ['S190425z', 'S200224ca']
+    mapfile = ['LALInference.fits.gz,0', 'S200224ca.fits']
+    e1 = sim_event(api_token, graceid[0], mapfile[0], [int(time[0]), \
+                      int(time[1]), int(time[2])])
+    e2 = sim_event(api_token, graceid[1], mapfile[1], [int(time[0]), \
+                      int(time[1]), int(time[2])])
+    print("S190425z:", e1)
+    print("S200224ca:", e2)
