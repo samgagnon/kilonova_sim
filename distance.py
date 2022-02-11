Fixed v sampler for obs, changed pDL formula

distance.py
@@ -46,13 +46,12 @@ def dp(D_0, D, v_0, v, m1, m2):
     Dd = D/D_0
     e = np.exp(-1/(2*d1**2) * (v/Dd - v_0)**2 - 
               1/(2*d2**2)*((1+v**2)/(2*Dd) - (1+v_0**2)/2)**2)
-    return Dd**2*e*H(D/D_0)*H(Dd)*H(Dmax/D_0 - Dd)*H(1-v**2)
+    return e*H(D/D_0)*H(Dd)*H(Dmax/D_0 - Dd)*H(1-v**2) # Dd**2* prefactor removed to undo low H0 bias
 
 
 def pDV_dist(D0, v0, m1, m2, plot=False):
-    thetaline = np.linspace(0, np.pi/2, 100)
-    vline = np.cos(thetaline)
-    dline = np.linspace(D0/3, D0*5, 100)
+    vline = np.linspace(0, 1, 1000)
+    dline = np.linspace(D0/3, D0*5, 1000)
     vv, dd = np.meshgrid(vline, dline)
     positions = np.vstack([vv.ravel(), dd.ravel()])
     values = np.vstack([vline, dline])
@@ -65,7 +64,7 @@ def pDV_dist(D0, v0, m1, m2, plot=False):
         fig.set_figheight(10)
         ax = plt.axes(projection='3d')
         surf = ax.plot_surface(vv, dd, pmesh/dps, rstride=1, cstride=1, cmap='coolwarm', edgecolor='none', zorder=1)
-        ax.plot_surface(vv, dd, np.ones((100, 100))*1e-5, rstride=1, cstride=1, cmap='coolwarm', edgecolor='none', zorder=2)
+        # ax.plot_surface(vv, dd, np.ones((1000, 1000))*1e-5, rstride=1, cstride=1, cmap='coolwarm', edgecolor='none', zorder=2)
         # ax.plot(0.3, 400, pm, 'k.', zorder=20)
         # ax.plot(0.34, 453, pm, 'kx', zorder=20)
         # ax.plot(0.3003308776117506, 400.5338672005339, pm, 'r.', zorder=20)
@@ -114,7 +113,7 @@ def D_vdu(d_true, v_true, m1, m2, v_guess, du, pdist):
     return pdist[-1][dloc], pdist[0][dloc, vloc], pdist[0].max(), pdist[0].sum()
 
 
-def p_DV(d_true, v_true, m1, m2, v_guess, d_guess):
+def p_DV(pdist, v_guess, d_guess):
     """
     -d_true: true event luminosity distance
     -v_true: true event angle variable
@@ -125,17 +124,20 @@ def p_DV(d_true, v_true, m1, m2, v_guess, d_guess):
     
     returns: probability of guess given prior
     """
-    pdist = pDV_dist(d_true, v_true, m1, m2)
     vloc = np.abs(pdist[3] - v_guess).argmin()
     dloc = np.abs(pdist[-1] - d_guess).argmin()
     am = pdist[0].argmax()
     
+    # import matplotlib.pyplot as plt
+    # plt.plot(pdist[-1], pdist[0][:,vloc])
+    # plt.axvline(d_guess)
+    # plt.show()
     ovloc = np.abs(pdist[3] - pdist[2][0][am]).argmin()
     odloc = np.abs(pdist[-1] - pdist[2][1][am]).argmin()
     return pdist[0][dloc, vloc], pdist[0].max(), pdist[0].sum()
 
 
-def v_from_CDF(d_true, v_true, m1, m2, dv):
+def v_from_CDF(pdist, dv):
     """
     Generates v from GW cdf
     -d_true: true event luminosity distance
@@ -147,12 +149,35 @@ def v_from_CDF(d_true, v_true, m1, m2, dv):
     returns: 
     -v: cosine of observation angle
     """
-    pdist = pDV_dist(d_true, v_true, m1, m2)
     vdist = np.sum(pdist[0], 0)
     vdist /= vdist.sum()
     vcdf = np.cumsum(vdist)
     vloc = np.abs(vcdf - dv).argmin()
     v = pdist[3][vloc]
+    return v
+
+def v_from_DCDF(pdist, DL, dv):
+    """
+    Generates v from GW cdf
+    -d_true: true event luminosity distance
+    -v_true: true event angle variable
+    -m1: true event m1
+    -m2: true event m2
+    -dv: random variable to determine v from inverse CDF
+
+    returns: 
+    -v: cosine of observation angle
+    """
+    dloc = np.abs(pdist[-1] - DL).argmin()
+    vdist = pdist[0][dloc]
+    vdist /= vdist.sum()
+    vcdf = np.cumsum(vdist)
+    vloc = np.abs(vcdf - dv).argmin()
+    v = pdist[3][vloc]
+    # import matplotlib.pyplot as plt
+    # plt.plot(pdist[3], vcdf)
+    # plt.axvline(v)
+    # plt.show()
     return v
 
 def m_from_dm(dm_tuple, event):
@@ -165,11 +190,11 @@ def m_from_dm(dm_tuple, event):
     mean_tuple = event[2]
     std_tuple = event[3]
     mx = np.linspace(1.1, 2.5, 1000)
-    gaus1 = np.exp((mx - mean_tuple[0])**2/(2*std_tuple[0]**2))
+    gaus1 = np.exp(-1*(mx - mean_tuple[0])**2/(2*std_tuple[0]**2))
     gcdf1 = np.cumsum(gaus1)
     i1 = np.abs(dm_tuple[0] - gcdf1).argmin()
     m1 = mx[i1]
-    gaus2 = np.exp((mx - mean_tuple[1])**2/(2*std_tuple[1]**2))
+    gaus2 = np.exp(-1*(mx - mean_tuple[1])**2/(2*std_tuple[1]**2))
     gcdf2 = np.cumsum(gaus2)
     i2 = np.abs(dm_tuple[1] - gcdf2).argmin()
     m2 = mx[i2]
@@ -177,9 +202,30 @@ def m_from_dm(dm_tuple, event):
 
 if __name__ == "__main__":
     from config import *
-    xr = np.linspace(0.7, 0.95, int(1e2))
-    vr = [v_from_CDF(DT1, 0.9, 1.4, 1.4, i) for i in xr]
-    p = [p_DV(DT1, 0.9, m1[0], m1[1], v, DT1) for v in vr]
-    e = np.array(p).argmax()
-    print(xr[e])
-    print(vr[e])
+    # xr = np.linspace(0.7, 0.95, int(1e2))
+    # vr = [v_from_CDF(DT1, 0.9, 1.4, 1.4, i) for i in xr]
+    # p = [p_DV(DT1, 0.9, m1[0], m1[1], v, DT1) for v in vr]
+    # e = np.array(p).argmax()
+    # print(xr[e])
+    # print(vr[e])
+    gen_pDV_dists(event_list, plot=True)
+    # import matplotlib.pyplot as plt
+    # from astropy import units as u
+    # from astropy import constants as c
+    # universe = cosmo.FlatLambdaCDM(70, 0.3)
+    # D0 = 40
+    # z = cosmo.z_at_value(universe.luminosity_distance, D0*u.Mpc)
+    # v0 = np.cos(np.linspace(0, np.pi/2, 5))
+    # # thetaline = np.linspace(0, np.pi, 1000)
+    # # vline = np.cos(thetaline)
+    # vline = np.linspace(-1, 1, 1000)
+    # dline = np.linspace(D0/3, D0*5, 1000)
+    # vv, dd = np.meshgrid(vline, dline)
+    # positions = np.vstack([vv.ravel(), dd.ravel()])
+    # values = np.vstack([vline, dline])
+    # for v in v0:
+    #     pmesh = dp(D0, dd, v, vv, 1.4, 1.4)
+    #     plt.plot(z*c.c.value/(dline*1e3), np.sum(pmesh, 1), label=str(np.arccos(v)*180/np.pi)+'$^\circ$')
+    # plt.axvline(70, color='r')
+    # plt.legend()
+    # plt.show()

forward.py
@@ -5,6 +5,7 @@ import astropy.units as u
 from scipy.stats import truncnorm
 from astropy.constants import c
 from astropy.cosmology import z_at_value
+from scipy.special import erfc
 
 from events import *
 from config import *
@@ -88,22 +89,21 @@ def light_curve(fitfunc, fixed_params):
 #     print(obs_probs)
 #     print(detect)
     detect = min(obs_mags) <= sig10
-#     print(True in detect)
 #     return (True in detect), pobs
     return detect, 1
 
 
 def nobs_forward(H0, ve, DL, pDL, event):
     # get free event parameters
-    v0 = v_from_CDF(event[1], event[4], event[2][0], event[2][1], ve[0])
-    # du = ve[1] # distance variable
+    pdist = pdict[str(event)]
+    v0 = v_from_CDF(pdist, ve[0])
     if k>2:
         M1, M2 = m_from_dm((ve[2], ve[3]), event)
     else:
         M1 = event[2][0]
         M2 = event[2][1]
     # use results from GWToolbox to get the true DL, M1, M2, v
-    TDL = event[1]
+    # TDL = event[1]
     # derived mass quantities
     if M1 < M2:
         Q = M1/M2
@@ -119,7 +119,7 @@ def nobs_forward(H0, ve, DL, pDL, event):
     fixed_params = {"ebv": 2.2, "rvhost": 3.1, "frad": 0.999, "nnhost": 1e18,\
               "texplosion": -0.01, "temperature": 2500, "kappa_red": 10,\
               "kappa_blue": 0.5, "kappagamma": 10000.0, "Mchirp": Mchirp,\
-              "q": Q, "cos_theta": v0, "cos_theta_open": 0.707107,\
+              "q": Q, "cos_theta": 1 - v0, "cos_theta_open": 0.707107,\
               "disk_frac": 0.15, "radius_ns": 11.0, "alpha": 1.0,\
               "Mtov": 2.2, "cos_theta_cocoon": 0.5, "tshock": 1.7,\
               "temperature_shock": 100, "lumdist": DL, "redshift": ZIC}
@@ -130,7 +130,7 @@ def nobs_forward(H0, ve, DL, pDL, event):
 
 def obs_forward(H0, ve, event):
     # get free event parameters
-    v0 = v_from_CDF(event[1], event[4], event[2][0], event[2][1], ve[0])
+    pdist = pdict[str(event)]
     if k>2:
         M1, M2 = m_from_dm((ve[1], ve[2]), event)
     else:
@@ -151,29 +151,29 @@ def obs_forward(H0, ve, event):
     universe = cosmo.FlatLambdaCDM(H0, 0.3)
     # "distance in cosmology"
     DIC = universe.luminosity_distance(TZ).value
+    v0 = v_from_DCDF(pdist, DIC, ve[0])
     # probability density of given cosmology-assigned distance at sampled angle v0
-    dprob, dpm, dps = p_DV(event[1], event[4], event[2][0], event[2][1], v0, DIC)
-    dprob /= dpm
+    # dprob, dpm, dps = p_DV(event[1], event[4], event[2][0], event[2][1], v0, DIC)
+    # dprob, dpm, dps = p_DV(pdist, v0, DIC)
+    # dprob /= dpm
     # print("dprob:", dprob)
 #     dprob = 1.0
-    # I also need to calculate the probability of this distance being allowed
-    # and return that probability to be used in the summary statistic
     fixed_params = {"ebv": 2.2, "rvhost": 3.1, "frad": 0.999, "nnhost": 1e18,\
               "texplosion": -0.01, "temperature": 2500, "kappa_red": 10,\
               "kappa_blue": 0.5, "kappagamma": 10000.0, "Mchirp": Mchirp,\
-              "q": Q, "cos_theta": v0, "cos_theta_open": 0.707107,\
+              "q": Q, "cos_theta": 1 - v0, "cos_theta_open": 0.707107,\
               "disk_frac": 0.15, "radius_ns": 11.0, "alpha": 1.0,\
               "Mtov": 2.2, "cos_theta_cocoon": 0.5, "tshock": 1.7,\
               "temperature_shock": 100, "lumdist": DIC, "redshift": TZ}
     det, pobs = light_curve(my_fitter, fixed_params)
-    return det, dprob #*dobs
+    return det, pobs
 
 
 def forward(v):
     # get terms from input vector
     H0 = v[0]
-    print("Input Vector:", v)
-    det_list = []
+    print("Input H0:", H0)
+    p = 0
     # first, loop through observations to see if observed distances make H0 illegal
     if obs_list is not None:
         i = 0
@@ -185,12 +185,27 @@ def forward(v):
             # "distance in cosmology"
             DIC = universe.luminosity_distance(event[0]).value
             # probability density of given cosmology-assigned distance at sampled angle v0
-            v0 = v_from_CDF(event[1], event[4], event[2][0], event[2][1], ve[0])
-            dprob, dpm, dps = p_DV(event[1], event[4], event[2][0], event[2][1], v0, DIC)
-            if dprob/dps < threshold:
-                return dict(x=np.array([0.0]))
+            pdist = pdict[str(event)]
+
+            # compute summary statistic
+            v0 = v_from_DCDF(pdist, DIC, ve[0])
+            vline = np.linspace(0, 1, 1000)
+            dline = np.linspace(DIC/3, DIC*5, 1000)
+            gi = np.abs(v0 - vline).argmin()
+            pslice = pdist[0][:, gi]
+            pslice /= pslice.sum()
+            mean = np.average(dline, weights=pslice)
+            var = np.average((dline-mean)**2, weights=pslice)
+            # print(DIC, mean, var)
+            p += (DIC-mean)**2/(2*var)
+            # p *= erfc(np.abs(DIC-mean)/np.sqrt(var))
+
+            dprob, dpm, dps = p_DV(pdist, v0, DIC)
+            # if dprob/dps < threshold:
+            #     return dict(x=np.array([0.0]))
     nobs_dl = []
     if nobs_list is not None:
+        # is this bit really necessary? I think not.
         i = 0
         ve_list = list(chunks(v[1:n_nobs*(k+1)+1], k+1))
         for event in nobs_list:
@@ -199,17 +214,15 @@ def forward(v):
             # generate a possible distance from the random variables du, v0
             # and the prior distribution (depending on true event parameters)
             pdist = pdict[str(event)]
-            v0 = v_from_CDF(event[1], event[4], event[2][0], event[2][1], ve[0])
+            # v0 = v_from_CDF(event[1], event[4], event[2][0], event[2][1], ve[0])
+            v0 = v_from_CDF(pdist, ve[0])
+
             DL, pDL, dpm, dps = D_vdu(event[1], event[4], event[2][0], event[2][1], v0, ve[1], pdist)
-
             # TODO: produce similar list for v in both this and obs_list
-
             nobs_dl += [[DL, pDL/dpm]]
-            print(pDL/dps)
             if pDL/dps < threshold:
                 return dict(x=np.array([0.0]))
     j = 0
-    p = 1
     if obs_list is not None:
         i = 0
         ve_list = list(chunks(v[n_nobs*(k+1)+1:], k))
@@ -220,45 +233,52 @@ def forward(v):
             if det is False:
                 print(det, 'bad')
                 return dict(x=np.array([0.0]))
-            det_list += [det]
-            p += pobs
+            # pdist = pdict[str(event)]
+            # dprob, dpm, dps = p_DV(pdist, v0, DIC)
+            # p += [pobs]
     if nobs_list is not None:
         i = 0
         ve_list = list(chunks(v[1:n_nobs*(k+1)+1], k+1))
         for event in nobs_list:
             ve = ve_list[i]
             i += 1
-#             print(nobs_dl[j])
             DL, pDL = nobs_dl[j]
             j += 1
             det, pobs = nobs_forward(H0, ve, DL, pDL, event)
             if det is True:
                 print(det, 'bad')
                 return dict(x=np.array([0.0]))
-            det_list += [det]
             # possibly remove pobs for nobs?
-            p += pobs
-    print((det_list == det_obs))
-    # if det_list == det_obs:
-        # print(p)
-        # x = [p]
-    # else:
-        # x = [0.0]
-    x = [p]
+            # p *= pobs
+    x = [erfc(np.sqrt(p/(n_obs-1)))]
     return dict(x=np.array(x))
 
 
 if __name__ == "__main__":
-    M1 = m11[0]
-    M2 = m11[1]
-    Q = M1/M2
-    Mchirp = ((M1*M2)**3/(M1+M2))**(1/5)
-    fixed_params = {"ebv": 2.2, "rvhost": 3.1, "frad": 0.999, "nnhost": 1e18,\
-              "texplosion": -0.01, "temperature": 2500, "kappa_red": 10,\
-              "kappa_blue": 0.5, "kappagamma": 10000.0, "Mchirp": Mchirp,\
-              "q": Q, "cos_theta": v11, "cos_theta_open": 0.707107,\
-              "disk_frac": 0.15, "radius_ns": 11.0, "alpha": 1.0,\
-              "Mtov": 2.2, "cos_theta_cocoon": 0.5, "tshock": 1.7,\
-              "temperature_shock": 100, "lumdist": DT11, "redshift": ZT11}
-    p = light_curve(my_fitter, fixed_params)
-    print(p)
+    # event = event_list[0]
+    # M1 = event[2][0]
+    # M2 = event[2][1]
+    # Q = M1/M2
+    # Mchirp = ((M1*M2)**3/(M1+M2))**(1/5)
+    # print(Q, Mchirp)
+    # print(event[0], event[1])
+    # fixed_params = {"ebv": 2.2, "rvhost": 3.1, "frad": 0.999, "nnhost": 1e18,\
+    #           "texplosion": -0.01, "temperature": 2500, "kappa_red": 10,\
+    #           "kappa_blue": 0.5, "kappagamma": 10000.0, "Mchirp": Mchirp,\
+    #           "q": Q, "cos_theta": 0.89, "cos_theta_open": 0.707107,\
+    #           "disk_frac": 0.15, "radius_ns": 11.0, "alpha": 1.0,\
+    #           "Mtov": 2.2, "cos_theta_cocoon": 0.5, "tshock": 1.7,\
+    #           "temperature_shock": 100, "lumdist": 141, "redshift": event[0]}
+    # p = light_curve(my_fitter, fixed_params)
+    # print(p)
+    a = 0
+    outlist = []
+    while a<1:
+        # h0 = np.random.uniform(65, 75)
+        h0 = 70
+        vu = np.random.uniform(0, 1, k*5+(k+1)*5)
+        v = np.asarray([h0]+list(vu))
+        pingas = forward(v)
+        print(v)
+        print(pingas)
+        a += 1

inference.py
@@ -26,7 +26,7 @@ low = np.array(low_list)
 high = np.array(high_list)
 prior = swyft.get_uniform_prior(low, high)
 
-observation_o = {'x': np.array([n_events])}
+observation_o = {'x': np.array([1.0])}
 
 n_observation_features = observation_o[observation_key].shape[0]
 observation_shapes = {key: value.shape for key, value in observation_o.items()}
@@ -82,7 +82,7 @@ def do_round_1d(bound, observation_focus):
     return posterior_1d, new_bound
 
 bound = None
-for i in range(3):
+for i in range(1):
     tic = time.time()
     posterior_1d, bound = do_round_1d(bound, observation_o)
     toc = time.time()